0435dda6c8be5cc20713249a6b1d8cdf88b2af89
Tom Merkle <tmerk1650@gmail.com>
2021-05-19 09:52:02 -0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
           height: "100%",
         }}
       >
-        get your mind back
+        mindback is an automation system designed for people. currently it is intended as a personal data management tool to collect, analyze, and then act on personal data. stay tuned for more updates.
       </div>
     );
   }
